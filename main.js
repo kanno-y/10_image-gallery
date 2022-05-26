fb7ec2113a5417d72450c53752dadb21b1e3608b
菅野 裕介 <y-kanno@A-069.local>
2022-05-26 16:18:47 +0900

    完成

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Declaring the array of image filenames */
+array = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+array.forEach((element) => {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", `images/${element}`);
+  newImage.addEventListener("click", () => {
+    displayedImage.setAttribute("src", `images/${element}`);
+  });
+  thumbBar.appendChild(newImage);
+});
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", trigger);
+
+/* Function */
+// オーバーレイ表示切り替え
+function trigger() {
+  attr = btn.getAttribute("class");
+  if (attr === "dark") {
+    btn.setAttribute("class", "light");
+    btn.innerHTML = "Lighten";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.innerHTML = "Darken";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+}
